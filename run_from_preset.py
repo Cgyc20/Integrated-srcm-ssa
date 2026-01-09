f60a7667c1d6e0b65e98d22d9738b78bd60b7915
Cgyc20 <cgyc20@bath.ac.uk>
2026-01-09 18:46:45 +0000

    Adding more yaml files

--- run_from_preset.py
+++ run_from_preset.py
@@ -3,15 +3,13 @@ from integrated_srcm_ssa.presets import load_system
 from srcm_engine.results.io import save_npz
 
 # Load system from YAML
-sim, cfg = load_system("presets/SI_model.yaml")
-
-# Run simulations
-res_ssa, meta_ssa = sim.run_ssa(**cfg["ssa"])
-res_hybrid, meta_hybrid = sim.run_hybrid(**cfg["hybrid"])
-
-# Choose output directory + filenames
+sim, cfg = load_system("presets/pred_prey.yaml")
 outdir = Path("results/my_experiment")
 outdir.mkdir(parents=True, exist_ok=True)
-
+# Run simulations
+res_ssa, meta_ssa = sim.run_ssa(**cfg["ssa"])
 save_npz(res_ssa, outdir / "ssa_run.npz", meta=meta_ssa)
+
+
+res_hybrid, meta_hybrid = sim.run_hybrid(**cfg["hybrid"])
 save_npz(res_hybrid, outdir / "hybrid_run.npz", meta=meta_hybrid)
